031cc0a9b8e6bc2a9948b54301382574bf842304
Frank Briceño García <frankbriceno2@gmail.com>
2021-06-11 04:44:05 +0200

    Actualización de scrip generador de la base de datos

--- api/config/data/database.sql
+++ api/config/data/database.sql
@@ -2,32 +2,27 @@ DROP DATABASE IF EXISTS `indigitall`;
 CREATE SCHEMA `indigitall` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE `indigitall`.`users` (
-  `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `fullname` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `language` VARCHAR(45) NOT NULL,
-  `latitude` FLOAT NOT NULL,
-  `longitude` FLOAT NOT NULL,
-  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  `update_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_user` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id_user`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `indigitall`.`friends` (
-  `id_friendship` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_user` INT UNSIGNED NOT NULL,
-  `id_user_friend` INT UNSIGNED NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `id_friendship` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int(10) unsigned NOT NULL,
+  `id_user_friend` int(10) unsigned NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id_friendship`),
-  INDEX `i_user_idx` (`id_user` ASC) VISIBLE,
-  CONSTRAINT `i_user`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `indigitall`.`users` (`id_user`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `i_user_friend`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `indigitall`.`users` (`id_user`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  UNIQUE KEY `u_frienship` (`id_user`,`id_user_friend`),
+  KEY `i_user_idx` (`id_user`),
+  CONSTRAINT `i_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `i_user_friend` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
